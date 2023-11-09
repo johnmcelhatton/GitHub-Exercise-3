59594b852279fe9c0a2f63b234b942bd1dd9c486
John McElhatton <jmcelhatton2010@gmail.com>
2023-11-09 10:52:10 -0500

    Initial Commit

---
+++ GitHub Exercise 3/GitHub_Exercise_3App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Exercise_3App.swift
+//  GitHub Exercise 3
+//
+//  Created by John McElhatton on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Exercise_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
